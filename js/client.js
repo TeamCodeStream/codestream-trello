31841342ed626c45ad7458c4be9e7a5c48abc59b
Peter Pezaris <pez@codestream.com>
2020-09-06 23:12:42 -0400

    go

--- js/client.js
+++ js/client.js
@@ -199,37 +199,6 @@ TrelloPowerUp.initialize({
 			},
 		];
 	},
-	"card-from-url": function (t, options) {
-		// options.url has the url in question
-		// if we know cool things about that url we can give Trello a name and desc
-		// to use when creating a card. Trello will also automatically add that url
-		// as an attachment to the created card
-		// As always you can return a Promise that resolves to the card details
-
-		return new Promise(function (resolve) {
-			resolve({
-				name: "💻 " + options.url + " 🤔",
-				desc: "This Power-Up knows cool things about the attached url",
-			});
-		});
-
-		// if we don't actually have any valuable information about the url
-		// we can let Trello know like so:
-		// throw t.NotHandled();
-	},
-	"format-url": function (t, options) {
-		// options.url has the url that we are being asked to format
-		// in our response we can include an icon as well as the replacement text
-
-		return {
-			icon: GRAY_ICON, // don't use a colored icon here
-			text: "👉 " + options.url + " 👈",
-		};
-
-		// if we don't actually have any valuable information about the url
-		// we can let Trello know like so:
-		// throw t.NotHandled();
-	},
 	"show-settings": function (t, options) {
 		// when a user clicks the gear icon by your Power-Up in the Power-Ups menu
 		// what should Trello show. We highly recommend the popup in this case as
@@ -239,55 +208,6 @@ TrelloPowerUp.initialize({
 			url: "./settings.html",
 			height: 184, // we can always resize later, but if we know the size in advance, its good to tell Trello
 		});
-	},
-
-	/*        
-      
-      🔑 Authorization Capabiltiies 🗝
-      
-      The following two capabilities should be used together to determine:
-      1. whether a user is appropriately authorized
-      2. what to do when a user isn't completely authorized
-      
-  */
-	"authorization-status": function (t, options) {
-		// Return a promise that resolves to an object with a boolean property 'authorized' of true or false
-		// The boolean value determines whether your Power-Up considers the user to be authorized or not.
-
-		// When the value is false, Trello will show the user an "Authorize Account" options when
-		// they click on the Power-Up's gear icon in the settings. The 'show-authorization' capability
-		// below determines what should happen when the user clicks "Authorize Account"
-
-		// For instance, if your Power-Up requires a token to be set for the member you could do the following:
-		return t.get("member", "private", "token").then(function (token) {
-			if (token) {
-				return { authorized: true };
-			}
-			return { authorized: false };
-		});
-		// You can also return the object synchronously if you know the answer synchronously.
-	},
-	"show-authorization": function (t, options) {
-		// Returns what to do when a user clicks the 'Authorize Account' link from the Power-Up gear icon
-		// which shows when 'authorization-status' returns { authorized: false }.
-
-		// If we want to ask the user to authorize our Power-Up to make full use of the Trello API
-		// you'll need to add your API from trello.com/app-key below:
-		let trelloAPIKey = "";
-		// This key will be used to generate a token that you can pass along with the API key to Trello's
-		// RESTful API. Using the key/token pair, you can make requests on behalf of the authorized user.
-
-		// In this case we'll open a popup to kick off the authorization flow.
-		if (trelloAPIKey) {
-			return t.popup({
-				title: "My Auth Popup",
-				args: { apiKey: trelloAPIKey }, // Pass in API key to the iframe
-				url: "./authorize.html", // Check out public/authorize.html to see how to ask a user to auth
-				height: 140,
-			});
-		} else {
-			console.log("🙈 Looks like you need to add your API key to the project!");
-		}
 	},
 });
 
